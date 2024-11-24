20a2465f004e2d75874ac64decdcb072cba9ac27
Jieun <lje2000lje@naver.com>
2024-11-25 01:52:22 +0900

    [chore] versionCode 및 versionName 업데이트

--- DaOnGil/app/build.gradle.kts
+++ DaOnGil/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         minSdk = 26
         //noinspection OldTargetApi,EditedTargetSdkVersion
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
