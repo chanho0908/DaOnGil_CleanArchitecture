b789ca521647b6c25993501b78537d013a9cc14b
chanho0908 <uni070@naver.com>
2024-11-25 22:46:05 +0900

    refactor : AppTheme Domain Layer Flow로 수정

--- DaOnGil/domain/src/main/java/kr/techit/lion/domain/repository/AppThemeRepository.kt
+++ DaOnGil/domain/src/main/java/kr/techit/lion/domain/repository/AppThemeRepository.kt
@@ -1,8 +1,9 @@
 package kr.techit.lion.domain.repository
 
+import kotlinx.coroutines.flow.Flow
 import kr.techit.lion.domain.model.AppTheme
 
 interface AppThemeRepository {
-    suspend fun getAppTheme(): AppTheme
+    fun getAppTheme(): Flow<AppTheme>
     suspend fun saveAppTheme(appTheme: AppTheme)
 }
