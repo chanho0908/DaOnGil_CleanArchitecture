44faf6cbcf58b7db0a3c79f6fbef67a0e6ea16e8
chanho0908 <uni070@naver.com>
2024-11-26 02:32:08 +0900

    feat : 나의 정보 가져올시 사용자 이름 추가

--- DaOnGil/domain/src/main/java/kr/techit/lion/domain/model/PersonalInfo.kt
+++ DaOnGil/domain/src/main/java/kr/techit/lion/domain/model/PersonalInfo.kt
@@ -1,6 +1,17 @@
 package kr.techit.lion.domain.model
 
 data class PersonalInfo (
-    val nickname: String = "",
-    val phone: String = ""
-)
+    val useName: String,
+    val nickname: String,
+    val phone: String
+){
+    companion object{
+        fun create(): PersonalInfo{
+            return PersonalInfo(
+                useName = "",
+                nickname = "",
+                phone = ""
+            )
+        }
+    }
+}
