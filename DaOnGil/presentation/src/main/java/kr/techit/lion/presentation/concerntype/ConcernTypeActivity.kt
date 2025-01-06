93a9cf159ccf04e83fdac05be8300ff0d7f45910
chanho0908 <uni070@naver.com>
2025-01-06 20:17:51 +0900

    feat : 관심 유형 화면 닉네임 intent로 받도록 수정

--- DaOnGil/presentation/src/main/java/kr/techit/lion/presentation/concerntype/ConcernTypeActivity.kt
+++ DaOnGil/presentation/src/main/java/kr/techit/lion/presentation/concerntype/ConcernTypeActivity.kt
@@ -1,14 +1,19 @@
 package kr.techit.lion.presentation.concerntype
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kr.techit.lion.presentation.R
+import kr.techit.lion.presentation.concerntype.vm.ConcernTypeViewModel
 
 @AndroidEntryPoint
 class ConcernTypeActivity : AppCompatActivity() {
+    private val viewModel: ConcernTypeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_concern_type)
+        val nickName = intent.getStringExtra("nickName")
+        nickName?.let { viewModel.setNickName(nickName) }
     }
 }
